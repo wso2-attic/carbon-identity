76154fbe05300f80307246c0f1bcc4f78d39d36b
Chamath Gunawardana <chamathg@gmail.com>
2014-04-22 16:37:31 +0530

    Adding ldap-server-manager feature

--- components/idp-mgt/org.wso2.carbon.idp.mgt.ui/src/main/resources/web/idpmgt/idp-mgt-edit-load.jsp
+++ components/idp-mgt/org.wso2.carbon.idp.mgt.ui/src/main/resources/web/idpmgt/idp-mgt-edit-load.jsp
@@ -1,33 +1,30 @@
 <!--
-~ Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+~ Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
 ~
 ~ WSO2 Inc. licenses this file to you under the Apache License,
 ~ Version 2.0 (the "License"); you may not use this file except
 ~ in compliance with the License.
 ~ You may obtain a copy of the License at
 ~
-~    http://www.apache.org/licenses/LICENSE-2.0
+~ http://www.apache.org/licenses/LICENSE-2.0
 ~
 ~ Unless required by applicable law or agreed to in writing,
 ~ software distributed under the License is distributed on an
 ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-~ KIND, either express or implied.  See the License for the
+~ KIND, either express or implied. See the License for the
 ~ specific language governing permissions and limitations
 ~ under the License.
 -->
+
+<%@page import="org.wso2.carbon.identity.application.common.model.idp.xsd.*"%>
 <%@ page import="org.apache.axis2.context.ConfigurationContext" %>
 <%@ page import="org.wso2.carbon.CarbonConstants" %>
-<%@ page import="org.wso2.carbon.idp.mgt.stub.dto.TrustedIdPDTO" %>
-<%@ page import="org.wso2.carbon.idp.mgt.ui.bean.TrustedIdPBean" %>
 <%@ page import="org.wso2.carbon.idp.mgt.ui.client.IdentityProviderMgtServiceClient" %>
-<%@ page import="org.wso2.carbon.idp.mgt.ui.util.IdentityProviderMgtUIUtil" %>
+<%@ page import="org.wso2.carbon.idp.mgt.ui.util.IdPManagementUIUtil" %>
 <%@ page import="org.wso2.carbon.ui.CarbonUIMessage" %>
 <%@ page import="org.wso2.carbon.ui.CarbonUIUtil" %>
 <%@ page import="org.wso2.carbon.utils.ServerConstants" %>
 <%@ page import="java.text.MessageFormat" %>
-<%@ page import="java.util.ArrayList" %>
-<%@ page import="java.util.Arrays" %>
-<%@ page import="java.util.HashMap" %>
 <%@ page import="java.util.ResourceBundle" %>
 
 <%
@@ -41,62 +38,11 @@
             ConfigurationContext configContext =
                     (ConfigurationContext) config.getServletContext().getAttribute(CarbonConstants.CONFIGURATION_CONTEXT);
             IdentityProviderMgtServiceClient client = new IdentityProviderMgtServiceClient(cookie, backendServerURL, configContext);
-            TrustedIdPDTO trustedIdPDTO = client.getIdPByName(idPName);
-            if(trustedIdPDTO != null){
-                TrustedIdPBean trustedIdPBean = new TrustedIdPBean();
-                trustedIdPBean.setIdPName(trustedIdPDTO.getIdPName());
-                trustedIdPBean.setPrimary(trustedIdPDTO.getPrimary());
-                trustedIdPBean.setHomeRealmId(trustedIdPDTO.getHomeRealmId());
-                if(trustedIdPDTO.getPublicCert() != null){
-                    trustedIdPBean.setCertData(IdentityProviderMgtUIUtil.getCertData(trustedIdPDTO.getPublicCert()));
-                }
-
-                if(trustedIdPDTO.getClaims() != null){
-                    trustedIdPBean.setClaims(new ArrayList<String>(Arrays.asList(trustedIdPDTO.getClaims())));
-                } else {
-                    trustedIdPDTO.setClaims(new String[0]);
-                    trustedIdPBean.setClaims(new ArrayList<String>());
-                }
-                if(trustedIdPDTO.getClaimMappings() != null){
-                    trustedIdPBean.setClaimMappings(IdentityProviderMgtUIUtil.getMappings(trustedIdPDTO.getClaimMappings()));
-                } else {
-                    trustedIdPDTO.setClaimMappings(new String[0]);
-                    trustedIdPBean.setClaimMappings(new HashMap<String, String>());
-                }
-
-                if(trustedIdPDTO.getRoles() != null){
-                    trustedIdPBean.setRoles(new ArrayList<String>(Arrays.asList(trustedIdPDTO.getRoles())));
-                } else {
-                    trustedIdPDTO.setRoles(new String[0]);
-                    trustedIdPBean.setRoles(new ArrayList<String>());
-                }
-                if(trustedIdPDTO.getRoleMappings() != null){
-                    trustedIdPBean.setRoleMappings(IdentityProviderMgtUIUtil.getMappings(trustedIdPDTO.getRoleMappings()));
-                } else {
-                    trustedIdPDTO.setRoleMappings(new String[0]);
-                    trustedIdPBean.setRoleMappings(new HashMap<String, String>());
-                }
-                trustedIdPBean.setTokenEndpointAlias(trustedIdPDTO.getTokenEndpointAlias());
-
-                trustedIdPBean.setSAML2SSOEnabled(trustedIdPDTO.getSAML2SSOEnabled());
-                trustedIdPBean.setIdpEntityId(trustedIdPDTO.getIdpEntityId());
-                trustedIdPBean.setSpEntityId(trustedIdPDTO.getSpEntityId());
-                trustedIdPBean.setSSOUrl(trustedIdPDTO.getSSOUrl());
-                trustedIdPBean.setAuthnRequestSigned(trustedIdPDTO.getAuthnRequestSigned());
-                trustedIdPBean.setLogoutEnabled(trustedIdPDTO.getLogoutEnabled());
-                trustedIdPBean.setLogoutRequestUrl(trustedIdPDTO.getLogoutRequestUrl());
-                trustedIdPBean.setLogoutRequestSigned(trustedIdPDTO.getLogoutRequestSigned());
-                trustedIdPBean.setAuthnResponseSigned(trustedIdPDTO.getAuthnResponseSigned());
-                trustedIdPBean.setOIDCEnabled(trustedIdPDTO.getOIDCEnabled());
-                trustedIdPBean.setClientId(trustedIdPDTO.getClientId());
-                trustedIdPBean.setClientSecret(trustedIdPDTO.getClientSecret());
-                trustedIdPBean.setAuthzEndpointUrl(trustedIdPDTO.getAuthzEndpointUrl());
-                trustedIdPBean.setTokenEndpointUrl(trustedIdPDTO.getTokenEndpointUrl());
-
-                session.setAttribute("trustedIdPDTO", trustedIdPDTO);
-                session.setAttribute("trustedIdPBean", trustedIdPBean);
+            FederatedIdentityProvider identityProvider = client.getIdPByName(idPName);
+            if(identityProvider != null){
+                session.setAttribute("identityProvider", identityProvider);
             }
-        } else if(session.getAttribute("tenantIdPList") == null){
+        } else if(session.getAttribute("identityProviderList") == null){
             response.sendRedirect("idp-mgt-list-load.jsp");
             return;
         }
--- components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/persistence/JDBCPersistenceManager.java
+++ components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/persistence/JDBCPersistenceManager.java
@@ -1,38 +1,40 @@
 /*
-*Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*WSO2 Inc. licenses this file to you under the Apache License,
-*Version 2.0 (the "License"); you may not use this file except
-*in compliance with the License.
-*You may obtain a copy of the License at
-*
-*http://www.apache.org/licenses/LICENSE-2.0
-*
-*Unless required by applicable law or agreed to in writing,
-*software distributed under the License is distributed on an
-*"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-*KIND, either express or implied.  See the License for the
-*specific language governing permissions and limitations
-*under the License.
-*/
+ *Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *WSO2 Inc. licenses this file to you under the Apache License,
+ *Version 2.0 (the "License"); you may not use this file except
+ *in compliance with the License.
+ *You may obtain a copy of the License at
+ *
+ *http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *Unless required by applicable law or agreed to in writing,
+ *software distributed under the License is distributed on an
+ *"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *KIND, either express or implied.  See the License for the
+ *specific language governing permissions and limitations
+ *under the License.
+ */
+
 package org.wso2.carbon.idp.mgt.persistence;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.idp.mgt.config.ConfigParser;
-import org.wso2.carbon.idp.mgt.exception.IdentityProviderMgtException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 import javax.xml.namespace.QName;
-import java.sql.Connection;
-import java.sql.SQLException;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.application.common.IdentityApplicationManagementException;
+import org.wso2.carbon.idp.mgt.config.IdPManagementConfig;
 
 /**
- * This class is used for handling trusted IdP meta data persistence in a JDBC Store.
- * It reads the data source properties from the JNDI name given in trusted-idp-config.xml.
+ * This class is used for handling Identity Provider data persistence in a JDBC Store.
+ * It reads the data source properties from the JNDI name given in idp-config.xml.
  * During the server start-up, it checks whether the database is created, if not it creates one.
  * This is implemented as a singleton. An instance of this class can be obtained through
  * JDBCPersistenceManager.getInstance() method.
@@ -45,13 +47,13 @@ public class JDBCPersistenceManager {
 
     /**
      * Get an instance of the JDBCPersistenceManager.
-     * It implements a lazy initialization with double checked locking,
-     * because it is initialized first by identity.core module during the start up.
+     * It implements a lazy initialization with double checked locking.
      *
      * @return JDBCPersistenceManager instance
      * @throws IdentityProviderMgtException Error when reading the data source configurations
      */
-    public static JDBCPersistenceManager getInstance() throws IdentityProviderMgtException {
+    public static JDBCPersistenceManager getInstance() throws IdentityApplicationManagementException {
+
         if (instance == null) {
             synchronized (JDBCPersistenceManager.class) {
                 if (instance == null) {
@@ -62,56 +64,67 @@ public class JDBCPersistenceManager {
         return instance;
     }
 
-    private JDBCPersistenceManager() throws IdentityProviderMgtException {
+    private JDBCPersistenceManager() throws IdentityApplicationManagementException {
         initDataSource();
     }
 
-    private void initDataSource() throws IdentityProviderMgtException {
+    private void initDataSource() throws IdentityApplicationManagementException {
+
         try {
-            String dataSourceName = ConfigParser.getInstance().getConfigElement("JDBCPersistenceManager")
-                    .getFirstChildWithName(new QName(ConfigParser.TRUSTED_IDP_DEFAULT_NAMESPACE, "DataSource"))
-                    .getFirstChildWithName(new QName(ConfigParser.TRUSTED_IDP_DEFAULT_NAMESPACE, "Name"))
+            String dataSourceName = IdPManagementConfig.getInstance().getConfigElement("JDBCPersistenceManager")
+                    .getFirstChildWithName(new QName(IdPManagementConfig.IDENTITY_PROVIDERS_DEFAULT_NAMESPACE, "DataSource"))
+                    .getFirstChildWithName(new QName(IdPManagementConfig.IDENTITY_PROVIDERS_DEFAULT_NAMESPACE, "Name"))
                     .getText();
             Context ctx = new InitialContext();
             dataSource = (DataSource) ctx.lookup(dataSourceName);
         }  catch (NamingException e) {
-            String errorMsg = "Error when looking up the IdP-Mgt data source";
-            log.error(errorMsg, e);
-            throw new IdentityProviderMgtException(errorMsg);
+            log.error(e.getMessage(), e);
+            String errorMsg = "Error occurred while looking up the Service Provider Management data source";
+            throw new IdentityApplicationManagementException(errorMsg);
         }  catch (Exception e) {
-            String errorMsg = "Error occurred while reading " + ConfigParser.TRSUTED_IDP_CONGIG;
-            log.error(errorMsg, e);
-            throw new IdentityProviderMgtException(errorMsg);
-        }
-    }
-
-    public void initializeDatabase() throws IdentityProviderMgtException {
-        TrustedIdPDBInitializer dbInitializer = new TrustedIdPDBInitializer(dataSource);
-        try {
-            dbInitializer.createTrustedIdPDB();
-        } catch (Exception e) {
-            String errorMsg = "Error when creating the Trusted IdP metadata store";
-            log.error(errorMsg, e);
-            throw new IdentityProviderMgtException(errorMsg);
+            log.error(e.getMessage(), e);
+            String errorMsg = "Error occurred while reading " + IdPManagementConfig.IDENTITY_PROVIDERS_CONGIG;
+            throw new IdentityApplicationManagementException(errorMsg);
         }
     }
 
     /**
-     * Returns a database connection for Teusted IdP metadata store.
+     * Get an instance of the JDBCPersistenceManager.
+     * It implements a lazy initialization with double checked locking.
+     *
+     * @throws IdentityProviderMgtException Error when initializing Identity Provider Management data store
+     */
+    public void initializeDatabase() throws IdentityApplicationManagementException {
+
+        IdPManagementDBInitializer dbInitializer = new IdPManagementDBInitializer(dataSource);
+        try {
+            dbInitializer.createIdentityProviderDB();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+            String errorMsg = "Error when creating the Service Provider Management data store";
+            throw new IdentityApplicationManagementException(errorMsg);
+        }
+    }
+
+    /**
+     * Returns a database connection for Identity Provider Management data store.
      *
      * @return Database connection
-     * @throws IdentityProviderMgtException Exception when getting DB connection on the data source.
+     * @throws IdentityProviderMgtException Error when getting DB connection
+     *         on the Identity Provider Management data source
      */
-    public Connection getDBConnection() throws IdentityProviderMgtException {
+    public Connection getDBConnection() throws IdentityApplicationManagementException {
+
         try {
             Connection dbConnection = dataSource.getConnection();
             dbConnection.setAutoCommit(false);
             dbConnection.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
             return dbConnection;
         } catch (SQLException e) {
-            String errorMsg = "Error occurred while trying to get a database connection from IdP-Mgt data source";
+            String errorMsg = "Error occurred while trying to get a database connection on " +
+                    "Service Provider Management data source";
             log.error(errorMsg, e);
-            throw new IdentityProviderMgtException(errorMsg);
+            throw new IdentityApplicationManagementException(errorMsg);
         }
     }
 
