52fcf481468bc8ef0c665ca6f6364cdea8db4f3b
Pulasthi Mahawithana <pulasthim@wso2.com>
2014-07-07 08:59:49 +0530

    Updated copyright

--- components/identity/org.wso2.carbon.identity.certificateauthority/src/main/java/org/wso2/carbon/identity/certificateauthority/data/CertAuthException.java
+++ components/identity/org.wso2.carbon.identity.certificateauthority/src/main/java/org/wso2/carbon/identity/certificateauthority/data/CertAuthException.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package org.wso2.carbon.identity.certificateauthority.data;
 
 
@@ -5,22 +23,18 @@ public class CertAuthException extends Exception {
 
     public CertAuthException() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
     public CertAuthException(String message, Throwable cause) {
         super(message, cause);
-        // TODO Auto-generated constructor stub
     }
 
     public CertAuthException(String message) {
         super(message);
-        // TODO Auto-generated constructor stub
     }
 
     public CertAuthException(Throwable cause) {
         super(cause);
-        // TODO Auto-generated constructor stub
     }
 
 }
