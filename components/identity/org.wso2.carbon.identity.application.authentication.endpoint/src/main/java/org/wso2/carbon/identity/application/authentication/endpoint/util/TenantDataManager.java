72f0f90e5a441191c775caeb61df915498bc2736
thariyarox <thariyarox@gmail.com>
2015-02-12 12:51:14 +0530

    Read TenantConfig.properties file from webapp if the file is not present in repository/conf

--- components/identity/org.wso2.carbon.identity.application.authentication.endpoint/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/TenantDataManager.java
+++ components/identity/org.wso2.carbon.identity.application.authentication.endpoint/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/util/TenantDataManager.java
@@ -27,7 +27,6 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.wso2.securevault.SecretResolver;
 import org.wso2.securevault.SecretResolverFactory;
-import org.wso2.securevault.SecurityConstants;
 import org.xml.sax.InputSource;
 
 import javax.xml.xpath.XPath;
@@ -67,74 +66,74 @@ public class TenantDataManager {
     private static final String RELATIVE_PATH_START_CHAR = ".";
     private static final String CHARACTER_ENCODING = "UTF-8";
 	private static final String CONFIG_RELATIVE_PATH = "./repository/conf/TenantConfig.properties";
+    private static final String CONFIG_FILE_NAME = "TenantConfig.properties";
     private static Properties prop;
     private static String carbonLogin = "";
     private static String serviceURL;
     private static String usernameHeaderName = "";
     private static List<String> tenantDomainList = new ArrayList<String>();
     private static boolean initialized = false;
-	private static boolean configFileExist = true;
+	private static boolean initAttempted = false;
 
     /**
      * Initialize Tenant data manager
      */
     public static synchronized void init() {
 
-		FileInputStream fileInputStream = null;
+        InputStream inputStream = null;
+        initAttempted = true;
 
-		try{
-        if (!initialized) {
-            prop = new Properties();
+        try {
+            if (!initialized) {
+                prop = new Properties();
                 String configFilePath = buildFilePath(CONFIG_RELATIVE_PATH);
                 File configFile = new File(configFilePath);
 
-                if(configFile.exists()){
-                    configFileExist = true;
-                    fileInputStream = new FileInputStream(configFile);
+                if (configFile.exists()) {
+                    log.info(CONFIG_FILE_NAME + " file loaded from " + CONFIG_RELATIVE_PATH);
+                    inputStream = new FileInputStream(configFile);
+                    prop.load(inputStream);
 
-                    prop.load(fileInputStream);
                     // Resolve encrypted properties with secure vault
                     resolveSecrets(prop);
-
-                    usernameHeaderName = getPropertyValue(USERNAME_HEADER);
-
-                    carbonLogin = getPropertyValue(USERNAME);
-
-                    // Base64 encoded username
-                    carbonLogin = new String(Base64.encode(carbonLogin.getBytes(CHARACTER_ENCODING)));
-
-                    String clientKeyStorePath = buildFilePath(getPropertyValue(CLIENT_KEY_STORE));
-                    String clientTrustStorePath = buildFilePath(getPropertyValue(CLIENT_TRUST_STORE));
-
-                    TenantMgtAdminServiceClient
-                            .loadKeyStore(clientKeyStorePath, getPropertyValue(CLIENT_KEY_STORE_PASSWORD));
-                    TenantMgtAdminServiceClient
-                            .loadTrustStore(clientTrustStorePath, getPropertyValue(CLIENT_TRUST_STORE_PASSWORD));
-                    TenantMgtAdminServiceClient.initMutualSSLConnection();
-
-                    // Build the service URL of tenant management admin service
-                    StringBuilder builder = new StringBuilder();
-                    serviceURL = builder.append(HTTPS_URL).append(getPropertyValue(HOST)).append(COLON)
-                            .append(getPropertyValue(PORT)).append(TENANT_MGT_ADMIN_SERVICE_URL).toString();
-
-                    initialized = true;
                 } else {
-                    configFileExist = false;
-
-                    if (log.isDebugEnabled()) {
-                        log.debug("Configuration file " + configFilePath + " not found");
-                    }
+                    log.info(CONFIG_FILE_NAME + " file loaded from authentication endpoint webapp");
+                    inputStream = TenantDataManager.class.getClassLoader().getResourceAsStream(CONFIG_FILE_NAME);
+                    prop.load(inputStream);
                 }
+
+                usernameHeaderName = getPropertyValue(USERNAME_HEADER);
+
+                carbonLogin = getPropertyValue(USERNAME);
+
+                // Base64 encoded username
+                carbonLogin = new String(Base64.encode(carbonLogin.getBytes(CHARACTER_ENCODING)));
+
+                String clientKeyStorePath = buildFilePath(getPropertyValue(CLIENT_KEY_STORE));
+                String clientTrustStorePath = buildFilePath(getPropertyValue(CLIENT_TRUST_STORE));
+
+                TenantMgtAdminServiceClient
+                        .loadKeyStore(clientKeyStorePath, getPropertyValue(CLIENT_KEY_STORE_PASSWORD));
+                TenantMgtAdminServiceClient
+                        .loadTrustStore(clientTrustStorePath, getPropertyValue(CLIENT_TRUST_STORE_PASSWORD));
+                TenantMgtAdminServiceClient.initMutualSSLConnection();
+
+                // Build the service URL of tenant management admin service
+                StringBuilder builder = new StringBuilder();
+                serviceURL = builder.append(HTTPS_URL).append(getPropertyValue(HOST)).append(COLON)
+                                    .append(getPropertyValue(PORT)).append(TENANT_MGT_ADMIN_SERVICE_URL).toString();
+
+                initialized = true;
             }
 
         } catch (Exception e) {
             log.error("Initialization failed : ", e);
         } finally {
-            if (fileInputStream != null) {
+            if (inputStream != null) {
                 try {
-                    fileInputStream.close();
+                    inputStream.close();
                 } catch (IOException e) {
-                    log.error("Failed to close the FileInputStream, file : " + CONFIG_RELATIVE_PATH, e);
+                    log.error("Failed to close the FileInputStream, file : " + CONFIG_FILE_NAME, e);
                 }
             }
         }
@@ -155,7 +154,7 @@ public class TenantDataManager {
         }
 
         if (log.isDebugEnabled()) {
-            log.debug("File path for KeyStore/TrustStore : " + path);
+            log.debug("File path constructed : " + path);
         }
         return path;
     }
@@ -309,14 +308,10 @@ public class TenantDataManager {
      * @return Tenant list enabled or disabled status
      */
     public static boolean isTenantListEnabled() {
-        if (configFileExist && !initialized) {
+        if (!initAttempted && !initialized) {
             init();
         }
-        if (configFileExist) {
-            return Boolean.parseBoolean(getPropertyValue(TENANT_LIST_ENABLED));
-        }
-
-        return false;
+        return Boolean.parseBoolean(getPropertyValue(TENANT_LIST_ENABLED));
     }
 
     /**
