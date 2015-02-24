728e5d45f2461d4c144cf143b11c8a3cc71e68ac
isuraD <isuradilhara@gmail.com>
2015-02-24 18:37:59 +0530

    Fixing IDENTITY-3057 and IDENTITY -3056

--- components/identity/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/processors/LogoutRequestProcessor.java
+++ components/identity/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/processors/LogoutRequestProcessor.java
@@ -174,25 +174,29 @@ public class LogoutRequestProcessor {
                         }
                         logoutReq.setIssuer(SAMLSSOUtil.getIssuer());
                         String logoutReqString = SAMLSSOUtil.encode(SAMLSSOUtil.marshall(logoutReq));
-                        logoutReqDTO.setLogoutResponse(logoutReqString);
-                        logoutReqDTO.setRpSessionId(rpSessionsList.get(key));
-                        singleLogoutReqDTOs[index] = logoutReqDTO;
-                        index++;
-                    } else {
-                        reqValidationResponseDTO.setIssuer(sessionsList.get(key).getIssuer());
-                        reqValidationResponseDTO.setAssertionConsumerURL(sessionsList.get(key)
-                                .getAssertionConsumerUrl());
-                        if (sessionsList.get(key).getLogoutURL() != null
-                                && sessionsList.get(key).getLogoutURL().length() > 0) {
-                            reqValidationResponseDTO.setAssertionConsumerURL(sessionsList.get(key)
-                                    .getLogoutURL());
-                        }
+						logoutReqDTO.setLogoutResponse(logoutReqString);
+						logoutReqDTO.setRpSessionId(rpSessionsList.get(key));
+						singleLogoutReqDTOs[index] = logoutReqDTO;
+						index++;
+					} else {
+						reqValidationResponseDTO.setIssuer(sessionsList.get(key).getIssuer());
+						reqValidationResponseDTO.setAssertionConsumerURL(sessionsList.get(key)
+								.getAssertionConsumerUrl());
+						if (sessionsList.get(key).getLogoutURL() != null
+								&& sessionsList.get(key).getLogoutURL().length() > 0) {
+							reqValidationResponseDTO.setAssertionConsumerURL(sessionsList.get(key)
+									.getLogoutURL());
+						}
+
                     }
                 }
                 reqValidationResponseDTO.setLogoutRespDTO(singleLogoutReqDTOs);
 
                 LogoutResponse logoutResponse = logoutMsgBuilder.buildLogoutResponse(
-                        logoutRequest.getID(), SAMLSSOConstants.StatusCodes.SUCCESS_CODE, null, sessionInfoData, logoutReqIssuer.isDoSignResponse());
+                        logoutRequest.getID(), SAMLSSOConstants.StatusCodes.SUCCESS_CODE, null,
+                        sessionInfoData, logoutReqIssuer.isDoSignResponse(),
+                        reqValidationResponseDTO.getAssertionConsumerURL());
+
                 reqValidationResponseDTO.setLogoutResponse(SAMLSSOUtil.encode(SAMLSSOUtil
                         .marshall(logoutResponse)));
                 reqValidationResponseDTO.setValid(true);
