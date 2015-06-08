c7874dc3b2a854b90ca2b5eee21b4c7f61b457f6
hpmtissera <hpmtissera@gmail.com>
2015-06-08 09:37:05 +0530

    refactor directory-server-manager

--- components/directory-server-manager/org.wso2.carbon.directory.server.manager.common/src/main/java/org/wso2/carbon/directory/server/manager/common/ServerPrinciple.java
+++ components/directory-server-manager/org.wso2.carbon.directory.server.manager.common/src/main/java/org/wso2/carbon/directory/server/manager/common/ServerPrinciple.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
 package org.wso2.carbon.directory.server.manager.common;
 
@@ -71,8 +71,32 @@ public class ServerPrinciple implements Comparable {
         this.serverPassword = serverPassword;
     }
 
+    @Override
     public int compareTo(Object o) {
         ServerPrinciple principle = (ServerPrinciple) o;
         return this.serverName.compareTo(principle.getServerName());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        ServerPrinciple that = (ServerPrinciple) o;
+
+        if (!serverName.equals(that.serverName)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return serverName.hashCode();
     }
 }
