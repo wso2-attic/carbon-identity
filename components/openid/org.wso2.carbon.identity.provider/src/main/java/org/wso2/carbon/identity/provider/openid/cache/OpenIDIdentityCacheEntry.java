e0c5da85721d1a85bcd4355caf0793cbcd1688c1
malithi <malithi.eamm@gmail.com>
2015-06-09 23:26:57 +0530

    code improvement and formatting

--- components/openid/org.wso2.carbon.identity.provider/src/main/java/org/wso2/carbon/identity/provider/openid/cache/OpenIDIdentityCacheEntry.java
+++ components/openid/org.wso2.carbon.identity.provider/src/main/java/org/wso2/carbon/identity/provider/openid/cache/OpenIDIdentityCacheEntry.java
@@ -1,5 +1,22 @@
-package org.wso2.carbon.identity.provider.openid.cache;
+/*
+ * Copyright (c) 2004-2005, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
+package org.wso2.carbon.identity.provider.openid.cache;
 
 import java.security.Key;
 import java.util.Arrays;
@@ -12,7 +29,6 @@ public class OpenIDIdentityCacheEntry extends OpenIDCacheEntry {
 
     private static final long serialVersionUID = 3746964700806693258L;
     private String cacheEntry;
-    //    private Set<String> cacheEntrySet;
     private String[] cacheEntryArray;
     private int hashEntry;
     private long cacheInterval;
@@ -41,10 +57,6 @@ public class OpenIDIdentityCacheEntry extends OpenIDCacheEntry {
         this.cacheEntryArray = Arrays.copyOf(cacheEntryArray, cacheEntryArray.length);
     }
 
-//    public OpenIDIdentityCacheEntry(Set<String> cacheEntrySet) {
-//        this.cacheEntrySet = cacheEntrySet;
-//    }
-
     public OpenIDIdentityCacheEntry(String cacheEntry, Key secretKey, Date date) {
         this.cacheEntry = cacheEntry;
         this.secretKey = secretKey;
@@ -62,10 +74,6 @@ public class OpenIDIdentityCacheEntry extends OpenIDCacheEntry {
     public long getCacheInterval() {
         return cacheInterval;
     }
-
-//    public Set<String> getCacheEntrySet() {
-//        return cacheEntrySet;
-//    }
 
     public boolean isCacheClearing() {
         return cacheClearing;
