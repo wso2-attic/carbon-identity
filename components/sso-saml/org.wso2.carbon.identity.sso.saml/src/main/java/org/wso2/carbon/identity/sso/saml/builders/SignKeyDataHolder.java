1109e190093cad7cb380dc1eed3a42e67aa95163
cdwijayarathna <cdwijayarathna@gmail.com>
2015-06-10 14:19:12 +0530

    code quality improvement saml-sso

--- components/sso-saml/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/builders/SignKeyDataHolder.java
+++ components/sso-saml/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/builders/SignKeyDataHolder.java
@@ -1,29 +1,28 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.identity.sso.saml.builders;
 
+import edu.emory.mathcs.backport.java.util.Collections;
 import org.apache.xml.security.signature.XMLSignature;
 import org.opensaml.xml.security.credential.Credential;
 import org.opensaml.xml.security.credential.CredentialContextSet;
 import org.opensaml.xml.security.credential.UsageType;
 import org.opensaml.xml.security.x509.X509Credential;
-import org.osgi.framework.BundleContext;
-import org.osgi.util.tracker.ServiceTracker;
 import org.wso2.carbon.base.ServerConfiguration;
 import org.wso2.carbon.context.PrivilegedCarbonContext;
 import org.wso2.carbon.core.util.KeyStoreManager;
@@ -32,7 +31,6 @@ import org.wso2.carbon.identity.core.util.IdentityUtil;
 import org.wso2.carbon.identity.sso.saml.SAMLSSOConstants;
 import org.wso2.carbon.identity.sso.saml.util.SAMLSSOUtil;
 import org.wso2.carbon.security.keystore.KeyStoreAdmin;
-import org.wso2.carbon.utils.AuthenticationObserver;
 import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
 
 import javax.crypto.SecretKey;
@@ -47,7 +45,9 @@ import java.util.Collection;
 
 public class SignKeyDataHolder implements X509Credential {
 
-    private static SignKeyDataHolder instance = null;
+    private static final String DSA_ENCRYPTION_ALGORITHM = "DSA";
+    public static final String SECURITY_KEY_STORE_KEY_ALIAS = "Security.KeyStore.KeyAlias";
+
     private String signatureAlgorithm = null;
     private X509Certificate[] issuerCerts = null;
 
@@ -110,13 +110,13 @@ public class SignKeyDataHolder implements X509Credential {
                 signatureAlgorithm = XMLSignature.ALGO_ID_SIGNATURE_RSA;
 
                 String pubKeyAlgo = issuerCerts[0].getPublicKey().getAlgorithm();
-                if (pubKeyAlgo.equalsIgnoreCase("DSA")) {
+                if (DSA_ENCRYPTION_ALGORITHM.equalsIgnoreCase(pubKeyAlgo)) {
                     signatureAlgorithm = XMLSignature.ALGO_ID_SIGNATURE_DSA;
                 }
 
             } else {
                 keyAlias = ServerConfiguration.getInstance().getFirstProperty(
-                        "Security.KeyStore.KeyAlias");
+                        SECURITY_KEY_STORE_KEY_ALIAS);
 
                 keyAdmin = new KeyStoreAdmin(tenantID,
                         SAMLSSOUtil.getRegistryService().getGovernanceSystemRegistry());
@@ -136,7 +136,7 @@ public class SignKeyDataHolder implements X509Credential {
                 signatureAlgorithm = XMLSignature.ALGO_ID_SIGNATURE_RSA;
 
                 String pubKeyAlgo = issuerCerts[0].getPublicKey().getAlgorithm();
-                if (pubKeyAlgo.equalsIgnoreCase("DSA")) {
+                if (DSA_ENCRYPTION_ALGORITHM.equalsIgnoreCase(pubKeyAlgo)) {
                     signatureAlgorithm = XMLSignature.ALGO_ID_SIGNATURE_DSA;
                 }
             }
@@ -155,51 +155,62 @@ public class SignKeyDataHolder implements X509Credential {
         this.signatureAlgorithm = signatureAlgorithm;
     }
 
+    @Override
     public Collection<X509CRL> getCRLs() {
-        return null;
+        return Collections.emptyList();
     }
 
+    @Override
     public X509Certificate getEntityCertificate() {
         return issuerCerts[0];
     }
 
+    @Override
     public Collection<X509Certificate> getEntityCertificateChain() {
         return Arrays.asList(issuerCerts);
     }
 
+    @Override
     public CredentialContextSet getCredentalContextSet() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
     public Class<? extends Credential> getCredentialType() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
     public String getEntityId() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
     public Collection<String> getKeyNames() {
         // TODO Auto-generated method stub
-        return null;
+        return Collections.emptyList();
     }
 
+    @Override
     public PrivateKey getPrivateKey() {
         return issuerPK;
     }
 
+    @Override
     public PublicKey getPublicKey() {
         return issuerCerts[0].getPublicKey();
     }
 
+    @Override
     public SecretKey getSecretKey() {
         // TODO Auto-generated method stub
         return null;
     }
 
+    @Override
     public UsageType getUsageType() {
         // TODO Auto-generated method stub
         return null;
--- components/sso-saml/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/builders/assertion/DefaultSAMLAssertionBuilder.java
+++ components/sso-saml/org.wso2.carbon.identity.sso.saml/src/main/java/org/wso2/carbon/identity/sso/saml/builders/assertion/DefaultSAMLAssertionBuilder.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.identity.sso.saml.builders.assertion;
 
 import org.apache.commons.logging.Log;
@@ -24,8 +24,33 @@ import org.joda.time.DateTime;
 import org.opensaml.Configuration;
 import org.opensaml.common.SAMLVersion;
 import org.opensaml.saml1.core.NameIdentifier;
-import org.opensaml.saml2.core.*;
-import org.opensaml.saml2.core.impl.*;
+import org.opensaml.saml2.core.Assertion;
+import org.opensaml.saml2.core.Attribute;
+import org.opensaml.saml2.core.AttributeStatement;
+import org.opensaml.saml2.core.AttributeValue;
+import org.opensaml.saml2.core.Audience;
+import org.opensaml.saml2.core.AudienceRestriction;
+import org.opensaml.saml2.core.AuthnContext;
+import org.opensaml.saml2.core.AuthnContextClassRef;
+import org.opensaml.saml2.core.AuthnStatement;
+import org.opensaml.saml2.core.Conditions;
+import org.opensaml.saml2.core.NameID;
+import org.opensaml.saml2.core.Subject;
+import org.opensaml.saml2.core.SubjectConfirmation;
+import org.opensaml.saml2.core.SubjectConfirmationData;
+import org.opensaml.saml2.core.impl.AssertionBuilder;
+import org.opensaml.saml2.core.impl.AttributeBuilder;
+import org.opensaml.saml2.core.impl.AttributeStatementBuilder;
+import org.opensaml.saml2.core.impl.AudienceBuilder;
+import org.opensaml.saml2.core.impl.AudienceRestrictionBuilder;
+import org.opensaml.saml2.core.impl.AuthnContextBuilder;
+import org.opensaml.saml2.core.impl.AuthnContextClassRefBuilder;
+import org.opensaml.saml2.core.impl.AuthnStatementBuilder;
+import org.opensaml.saml2.core.impl.ConditionsBuilder;
+import org.opensaml.saml2.core.impl.NameIDBuilder;
+import org.opensaml.saml2.core.impl.SubjectBuilder;
+import org.opensaml.saml2.core.impl.SubjectConfirmationBuilder;
+import org.opensaml.saml2.core.impl.SubjectConfirmationDataBuilder;
 import org.opensaml.xml.schema.XSString;
 import org.opensaml.xml.schema.impl.XSStringBuilder;
 import org.wso2.carbon.identity.base.IdentityException;
@@ -47,6 +72,7 @@ public class DefaultSAMLAssertionBuilder implements SAMLAssertionBuilder {
 
     @Override
     public void init() throws IdentityException {
+        //Overridden method, no need to implement the body
     }
 
     @Override
@@ -139,7 +165,7 @@ public class DefaultSAMLAssertionBuilder implements SAMLAssertionBuilder {
             Map<String, String> claims = SAMLSSOUtil.getAttributes(authReqDTO);
             if (claims != null && !claims.isEmpty()) {
                 AttributeStatement attrStmt = buildAttributeStatement(claims);
-                if(attrStmt != null) {
+                if (attrStmt != null) {
                     samlAssertion.getAttributeStatements().add(attrStmt);
                 }
             }
@@ -184,17 +210,18 @@ public class DefaultSAMLAssertionBuilder implements SAMLAssertionBuilder {
         }
 
         AttributeStatement attStmt = new AttributeStatementBuilder().buildObject();
-        Iterator<String> ite = claims.keySet().iterator();
+        Iterator<Map.Entry<String, String>> iterator = claims.entrySet().iterator();
         boolean atLeastOneNotEmpty = false;
         for (int i = 0; i < claims.size(); i++) {
-            String claimUri = ite.next();
-            String claimValue = claims.get(claimUri);
-            if(claimUri != null && !claimUri.trim().isEmpty() && claimValue != null && !claimValue.trim().isEmpty()) {
+            Map.Entry<String, String> claimEntry = iterator.next();
+            String claimUri = claimEntry.getKey();
+            String claimValue = claimEntry.getValue();
+            if (claimUri != null && !claimUri.trim().isEmpty() && claimValue != null && !claimValue.trim().isEmpty()) {
                 atLeastOneNotEmpty = true;
-                Attribute attrib = new AttributeBuilder().buildObject();
-                attrib.setName(claimUri);
+                Attribute attribute = new AttributeBuilder().buildObject();
+                attribute.setName(claimUri);
                 //setting NAMEFORMAT attribute value to basic attribute profile
-                attrib.setNameFormat(SAMLSSOConstants.NAME_FORMAT_BASIC);
+                attribute.setNameFormat(SAMLSSOConstants.NAME_FORMAT_BASIC);
                 // look
                 // https://wiki.shibboleth.net/confluence/display/OpenSAML/OSTwoUsrManJavaAnyTypes
                 XSStringBuilder stringBuilder = (XSStringBuilder) Configuration.getBuilderFactory().
@@ -209,19 +236,19 @@ public class DefaultSAMLAssertionBuilder implements SAMLAssertionBuilder {
                         if (attValue != null && attValue.trim().length() > 0) {
                             stringValue = stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
                             stringValue.setValue(attValue);
-                            attrib.getAttributeValues().add(stringValue);
+                            attribute.getAttributeValues().add(stringValue);
                         }
                     }
                 } else {
                     stringValue = stringBuilder.buildObject(AttributeValue.DEFAULT_ELEMENT_NAME, XSString.TYPE_NAME);
                     stringValue.setValue(claimValue);
-                    attrib.getAttributeValues().add(stringValue);
+                    attribute.getAttributeValues().add(stringValue);
                 }
 
-                attStmt.getAttributes().add(attrib);
+                attStmt.getAttributes().add(attribute);
             }
         }
-        if(atLeastOneNotEmpty){
+        if (atLeastOneNotEmpty) {
             return attStmt;
         } else {
             return null;
