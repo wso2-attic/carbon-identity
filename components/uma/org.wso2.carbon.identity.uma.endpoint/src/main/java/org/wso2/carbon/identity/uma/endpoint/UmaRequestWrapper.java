47e36cfe81bd1e632951ea2ab1eae393d8f6f86d
Farasath Ahamed <mefarazath@gmail.com>
2015-06-15 17:18:38 +0530

    Authz endpoint impl

---
+++ components/uma/org.wso2.carbon.identity.uma.endpoint/src/main/java/org/wso2/carbon/identity/uma/endpoint/UmaRequestWrapper.java
@@ -0,0 +1,67 @@
+/*
+ *
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * /
+ */
+
+package org.wso2.carbon.identity.uma.endpoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.ws.rs.core.MultivaluedMap;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UmaRequestWrapper extends HttpServletRequestWrapper {
+
+    private MultivaluedMap<String,String> form;
+    private Enumeration<String> parameterNames;
+
+    public UmaRequestWrapper(HttpServletRequest request,MultivaluedMap<String,String> form) {
+        super(request);
+        this.form = form;
+
+        Set<String> parameterNameSet = new HashSet<String>();
+        // Add post parameters
+        parameterNameSet.addAll(form.keySet());
+        // Add servlet request parameters
+        Enumeration<String> parameterNames = request.getParameterNames();
+
+        while (parameterNames.hasMoreElements()) {
+            parameterNameSet.add(parameterNames.nextElement());
+        }
+
+        this.parameterNames = Collections.enumeration(parameterNameSet);
+    }
+
+    @Override
+    public String getParameter(String name) {
+        String value = super.getParameter(name);
+        if (value == null) {
+            value = form.getFirst(name);
+        }
+        return value;
+    }
+
+    @Override
+    public Enumeration<String> getParameterNames() {
+        return parameterNames;
+    }
+
+}
---
+++ components/uma/org.wso2.carbon.identity.uma.endpoint/src/main/java/org/wso2/carbon/identity/uma/endpoint/authorization/UmaAuthorizationEndpoint.java
@@ -0,0 +1,76 @@
+/*
+ *
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * /
+ */
+
+package org.wso2.carbon.identity.uma.endpoint.authorization;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.cxf.jaxrs.ext.multipart.Multipart;
+import org.apache.cxf.jaxrs.impl.ResponseImpl;
+import org.wso2.carbon.context.PrivilegedCarbonContext;
+import org.wso2.carbon.identity.uma.endpoint.UmaRequestWrapper;
+import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+@Path("/rpt")
+public class UmaAuthorizationEndpoint {
+
+    private static final Log log = LogFactory.getLog(UmaAuthorizationEndpoint.class);
+
+    @POST
+    @Path("/")
+    @Consumes("application/json")
+    @Produces("application/json")
+    public Response issueRPT(@Context HttpServletRequest request,@Multipart(value = "application/json") String jsonPayload){
+
+        PrivilegedCarbonContext.startTenantFlow();
+        PrivilegedCarbonContext carbonContext = PrivilegedCarbonContext.getThreadLocalCarbonContext();
+        carbonContext.setTenantId(MultitenantConstants.SUPER_TENANT_ID);
+        carbonContext.setTenantDomain(MultitenantConstants.SUPER_TENANT_DOMAIN_NAME);
+
+        String jsonPayLoad = jsonPayload;
+        System.out.println();
+        HttpServletRequestWrapper httpServletRequestWrapper = new UmaRequestWrapper(request,null);
+
+        // log the RPT Token Request
+        if(log.isDebugEnabled()){
+            logRptRequest(httpServletRequestWrapper);
+        }
+
+        return null;
+    }
+
+
+    private void logRptRequest(HttpServletRequest httpServletRequest){
+
+        StringBuilder builder = new StringBuilder();
+
+    }
+}
