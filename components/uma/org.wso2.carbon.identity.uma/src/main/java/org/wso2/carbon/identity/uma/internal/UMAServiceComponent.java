6fe683faae789a090f2e2a99945d542e15c75337
Farasath Ahamed <mefarazath@gmail.com>
2015-06-16 00:31:03 +0530

    UMA Service component impl

---
+++ components/uma/org.wso2.carbon.identity.uma/src/main/java/org/wso2/carbon/identity/uma/internal/UMAServiceComponent.java
@@ -0,0 +1,88 @@
+/*
+ *
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * /
+ */
+
+package org.wso2.carbon.identity.uma.internal;
+
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.ComponentContext;
+import org.wso2.carbon.identity.uma.UMAService;
+import org.wso2.carbon.registry.api.RegistryService;
+import org.wso2.carbon.user.core.service.RealmService;
+
+/**
+ * @scr.component immediate="true" activate="activate"
+ */
+public class UMAServiceComponent {
+    private static Log log = LogFactory.getLog(UMAServiceComponent.class);
+    private static BundleContext bundleContext;
+
+
+    protected void activate(ComponentContext context){
+        // register UMAService as an OSGIService
+        bundleContext = context.getBundleContext();
+        bundleContext.registerService(UMAService.class.getName(),new UMAService(),null);
+
+        // expose the UMA Server Configuration as an OSGIService
+
+        if (log.isDebugEnabled()){
+            log.debug("Identity UMA bundle activated");
+        }
+    }
+
+    protected void deactivate(ComponentContext context){
+        if (log.isDebugEnabled()){
+            log.debug("Identity UMA bundle deactivated");
+        }
+    }
+
+    protected void setRegistryService(RegistryService registryService) {
+        if (log.isDebugEnabled()) {
+            log.info("RegistryService set in Identity UMA bundle");
+        }
+        UMAServiceComponentHolder.setRegistryService(registryService);
+    }
+
+    protected void unsetRegistryService(RegistryService registryService) {
+        if (log.isDebugEnabled()) {
+            log.info("RegistryService unset in Identity UMA bundle");
+        }
+        UMAServiceComponentHolder.setRegistryService(null);
+    }
+
+    protected void setRealmService(RealmService realmService) {
+        if (log.isDebugEnabled()) {
+            log.info("Setting the Realm Service");
+        }
+        UMAServiceComponentHolder.setRealmService(realmService);
+    }
+
+    protected void unsetRealmService(RealmService realmService) {
+        if (log.isDebugEnabled()) {
+            log.info("Unsetting the Realm Service");
+        }
+        UMAServiceComponentHolder.setRealmService(null);
+    }
+
+
+
+}
---
+++ components/uma/org.wso2.carbon.identity.uma/src/main/java/org/wso2/carbon/identity/uma/internal/UMAServiceComponentHolder.java
@@ -0,0 +1,50 @@
+/*
+ *
+ * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ * /
+ */
+
+package org.wso2.carbon.identity.uma.internal;
+
+import org.wso2.carbon.registry.api.RegistryService;
+import org.wso2.carbon.user.core.service.RealmService;
+
+public class UMAServiceComponentHolder {
+    private static RegistryService registryService;
+    private static RealmService realmService;
+
+    private UMAServiceComponentHolder(){
+
+    }
+
+    public static RegistryService getRegistryService() {
+        return registryService;
+    }
+
+    public static void setRegistryService(RegistryService registryService) {
+        UMAServiceComponentHolder.registryService = registryService;
+    }
+
+    public static RealmService getRealmService() {
+        return realmService;
+    }
+
+    public static void setRealmService(RealmService realmService) {
+        UMAServiceComponentHolder.realmService = realmService;
+    }
+
+}
