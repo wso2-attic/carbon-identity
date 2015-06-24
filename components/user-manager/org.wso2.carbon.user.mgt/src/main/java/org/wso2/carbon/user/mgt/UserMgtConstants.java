4c3d86de5f9a4dc87e489d7785f875785258ca8e
Damith Senanayake <dracusds123@gmail.com>
2015-06-24 15:41:57 +0530

    refactored org.wso2.carbon.user.mgt

--- components/user-manager/org.wso2.carbon.user.mgt/src/main/java/org/wso2/carbon/user/mgt/UserMgtConstants.java
+++ components/user-manager/org.wso2.carbon.user.mgt/src/main/java/org/wso2/carbon/user/mgt/UserMgtConstants.java
@@ -1,25 +1,30 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2010 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.wso2.carbon.user.mgt;
 
 import org.wso2.carbon.CarbonConstants;
 
 public class UserMgtConstants {
+
+    private UserMgtConstants(){
+
+    }
 
     public static final String SYSTEM_RESOURCE = "System";
 
@@ -34,6 +39,6 @@ public class UserMgtConstants {
 
     public static final String INTERNAL_ROLE = "Internal";
 
-    public static final String EXTERNAL_ROLE = "External";      
+    public static final String EXTERNAL_ROLE = "External";
     //component xml constants
 }
