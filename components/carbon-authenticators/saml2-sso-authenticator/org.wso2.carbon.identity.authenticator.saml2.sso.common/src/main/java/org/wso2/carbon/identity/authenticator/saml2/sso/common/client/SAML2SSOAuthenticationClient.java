d2e067ba1e406a8e515cfc584862f93c1946ca96
cdwijayarathna <cdwijayarathna@gmail.com>
2015-07-03 17:22:15 +0530

    Revert "IDENTITY-3389 Move dependency versions from component level poms to master pom"
    This reverts commit 8806a197ded1f0ce02134f15db1d765e8be240e0.

--- components/carbon-authenticators/saml2-sso-authenticator/org.wso2.carbon.identity.authenticator.saml2.sso.common/src/main/java/org/wso2/carbon/identity/authenticator/saml2/sso/common/client/SAML2SSOAuthenticationClient.java
+++ components/carbon-authenticators/saml2-sso-authenticator/org.wso2.carbon.identity.authenticator.saml2.sso.common/src/main/java/org/wso2/carbon/identity/authenticator/saml2/sso/common/client/SAML2SSOAuthenticationClient.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.wso2.carbon.identity.authenticator.saml2.sso.ui.client;
+package org.wso2.carbon.identity.authenticator.saml2.sso.common.client;
 
 import org.apache.axis2.client.Options;
 import org.apache.axis2.client.ServiceClient;
