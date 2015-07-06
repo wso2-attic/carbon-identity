9e1216e8a08bb927e1059c6e50f4e9e1c1f92e72
indunil.uom@gmail.com <indunil.uom@gmail.com>
2015-07-06 05:55:01 +0530

    Resolved IDENTITY-2900 for Google

--- components/application-authenticators/org.wso2.carbon.identity.application.authenticator.social/src/main/java/org/wso2/carbon/identity/application/authenticator/social/google/GoogleOAuth2Authenticator.java
+++ components/application-authenticators/org.wso2.carbon.identity.application.authenticator.social/src/main/java/org/wso2/carbon/identity/application/authenticator/social/google/GoogleOAuth2Authenticator.java
@@ -15,7 +15,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-package org.wso2.carbon.identity.application.authenticator.oidc.googleext;
+package org.wso2.carbon.identity.application.authenticator.social.google;
 
 import org.apache.amber.oauth2.client.OAuthClient;
 import org.apache.amber.oauth2.client.URLConnectionClient;
