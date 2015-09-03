f9c093f5b97e8a2f58adf2d7f2ed731b8171f84e
IndunilRathnayake <indunil@wso2.com>
2015-09-03 14:02:56 +0530

    added some chnages in code

--- components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/IdentityProviderManagementService.java
+++ components/idp-mgt/org.wso2.carbon.idp.mgt/src/main/java/org/wso2/carbon/idp/mgt/IdentityProviderManagementService.java
@@ -19,7 +19,7 @@
 package org.wso2.carbon.idp.mgt;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.context.CarbonContext;
