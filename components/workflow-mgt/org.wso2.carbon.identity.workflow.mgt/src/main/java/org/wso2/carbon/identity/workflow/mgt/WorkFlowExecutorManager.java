45a83436ab3b662ead2f6fdf5fd895d31421f68e
cdwijayarathna <cdwijayarathna@gmail.com>
2015-09-04 15:39:25 +0530

    IDENTITY-3501 UI in Management Console to Check Workflow Status - changing table name

--- components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/WorkFlowExecutorManager.java
+++ components/workflow-mgt/org.wso2.carbon.identity.workflow.mgt/src/main/java/org/wso2/carbon/identity/workflow/mgt/WorkFlowExecutorManager.java
@@ -126,13 +126,13 @@ public class WorkFlowExecutorManager {
         if (request != null) {
             WorkflowRequestDAO workflowRequestDAO = new WorkflowRequestDAO();
             String requestId = request.getUuid();
+            workflowRequestAssociationDAO.updateStatusOfRelationship(requestWorkflowId, status);
+            workflowRequestDAO.updateLastUpdatedTimeOfRequest(requestId);
             if (StringUtils.isNotBlank(requestWorkflowId) && workflowRequestDAO.retrieveStatusOfWorkflow(request
                     .getUuid()).equals(WorkflowRequestStatus.DELETED.toString())) {
                 log.info("Callback received for request " + requestId + " which is already deleted by user. ");
                 return;
             }
-            workflowRequestAssociationDAO.updateStatusOfRelationship(requestWorkflowId, status);
-            workflowRequestDAO.updateLastUpdatedTimeOfRequest(requestId);
             if (status.equals(WorkflowRequestStatus.APPROVED.toString()) && !isAllWorkflowsCompleted
                     (workflowRequestAssociationDAO, requestId)) {
                 return;
