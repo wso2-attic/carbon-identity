df5b40a6b81d24624145c5c6b0688922068097ee
IndunilR <indunil@wso2.com>
2015-09-25 13:43:23 +0530

    Multi Tenant Support and Handler wise Implementation for Identity Management

---
+++ components/identity-mgt-new/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/IdentityMgtConfig.java
@@ -0,0 +1,136 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.mgt;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.wso2.carbon.identity.mgt.service.IdentityMgtService;
+import org.wso2.carbon.identity.mgt.service.IdentityMgtServiceImpl;
+import org.wso2.carbon.utils.multitenancy.MultitenantConstants;
+
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+
+/**
+ * encapsulates recovery config data
+ */
+public class IdentityMgtConfig {
+
+    private static final Log log = LogFactory.getLog(IdentityMgtConfig.class);
+    private boolean accountLockEnable;
+    IdentityMgtService identityMgtService = new IdentityMgtServiceImpl();
+    private boolean enableAuthPolicy;
+    private boolean authPolicyAccountExistCheck;
+    private int authPolicyMaxLoginAttempts;
+    private int notificationExpireTime;
+
+    public Properties addConfiguration(int tenantId) throws IdentityMgtException{
+
+        Properties properties = null;
+        try {
+            properties = identityMgtService.addConfiguration(tenantId);
+        } catch (IdentityMgtException ex) {
+            throw new IdentityMgtException("Error when adding configurations", ex);
+        }
+        return properties;
+    }
+
+    public Properties getConfiguration(int tenantId) throws IdentityMgtException{
+
+        Properties properties = new Properties();
+        Map<String, String> configMap = null;
+        try {
+            configMap = identityMgtService.getConfiguration(tenantId);
+        } catch (IdentityMgtException ex) {
+            throw new IdentityMgtException("Error when retrieving configurations", ex);
+        }
+        Iterator<Map.Entry<String, String>> iterator = configMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> pair = iterator.next();
+            properties.setProperty(pair.getKey(), pair.getValue());
+        }
+        return properties;
+
+    }
+
+    public void setConfiguration(Properties properties) {
+
+        if (!properties.isEmpty()) {
+
+            String authPolicyAccountExistCheck = properties.
+                    getProperty(IdentityMgtConstants.PropertyConfig.AUTH_POLICY_ACCOUNT_EXIST);
+            if (authPolicyAccountExistCheck != null) {
+                this.authPolicyAccountExistCheck = Boolean.parseBoolean(authPolicyAccountExistCheck.trim());
+            }
+
+            String enableAuthPolicy = properties.
+                    getProperty(IdentityMgtConstants.PropertyConfig.AUTH_POLICY_ENABLE);
+            if (enableAuthPolicy != null) {
+                this.enableAuthPolicy = Boolean.parseBoolean(enableAuthPolicy.trim());
+            }
+
+            String accountLockEnable = properties.
+                    getProperty(IdentityMgtConstants.PropertyConfig.ACCOUNT_LOCK_ENABLE);
+            if (accountLockEnable != null) {
+                this.accountLockEnable = Boolean.parseBoolean(accountLockEnable.trim());
+            }
+
+            String maxLoginAttemptProperty = properties.
+                    getProperty(IdentityMgtConstants.PropertyConfig.AUTH_POLICY_ACCOUNT_LOCKING_FAIL_ATTEMPTS);
+            if (maxLoginAttemptProperty != null) {
+                this.authPolicyMaxLoginAttempts = Integer.valueOf(maxLoginAttemptProperty.trim());
+            }
+
+            if (this.authPolicyMaxLoginAttempts == 0) {
+                // default value is set
+                this.authPolicyMaxLoginAttempts = 10;
+            }
+
+            String notificationExpireTimeProperty = properties.
+                    getProperty(IdentityMgtConstants.PropertyConfig.NOTIFICATION_LINK_EXPIRE_TIME);
+            if (notificationExpireTimeProperty != null) {
+                this.notificationExpireTime = Integer.parseInt(notificationExpireTimeProperty.trim());
+            }
+        }
+    }
+
+    public boolean isAccountLockEnable() {
+        return accountLockEnable;
+    }
+
+    public boolean isAuthPolicyAccountExistCheck() {
+        return authPolicyAccountExistCheck;
+    }
+
+    public boolean isEnableAuthPolicy() {
+        return enableAuthPolicy;
+    }
+
+    public int getAuthPolicyMaxLoginAttempts() {
+        return authPolicyMaxLoginAttempts;
+    }
+
+    public int getNotificationExpireTime() {
+        return notificationExpireTime;
+    }
+
+}
---
+++ components/identity-mgt-new/org.wso2.carbon.identity.mgt/src/main/java/org/wso2/carbon/identity/mgt/handler/internal/AccountLockEventHandler.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.mgt.handler.internal;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.identity.base.IdentityException;
+import org.wso2.carbon.identity.core.model.IdentityErrorMsgContext;
+import org.wso2.carbon.identity.core.util.IdentityUtil;
+import org.wso2.carbon.identity.mgt.IdentityMgtConfig;
+import org.wso2.carbon.identity.mgt.IdentityMgtConstants;
+import org.wso2.carbon.identity.mgt.IdentityMgtException;
+import org.wso2.carbon.identity.mgt.model.UserIdentityClaim;
+import org.wso2.carbon.identity.mgt.event.IdentityMgtEvent;
+import org.wso2.carbon.identity.mgt.store.UserIdentityDataStore;
+import org.wso2.carbon.user.core.UserCoreConstants;
+import org.wso2.carbon.user.core.UserStoreException;
+import org.wso2.carbon.user.core.UserStoreManager;
+import org.wso2.carbon.user.core.util.UserCoreUtil;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+public class AccountLockEventHandler extends AbstractEventHandler {
+
+    private static final Log log = LogFactory.getLog(AccountLockEventHandler.class);
+
+    @Override
+    public void init() {
+
+        registeredEventList = new ArrayList<String>() {{
+            add(IdentityMgtConstants.Event.PRE_AUTHENTICATION);
+        }};
+    }
+
+    @Override
+    public boolean handleEvent(IdentityMgtEvent identityMgtEvent) throws IdentityMgtException {
+
+        Map<String, Object> properties = identityMgtEvent.getEventProperties();
+
+        String userName = (String) properties.get(IdentityMgtConstants.EventProperty.USER_NAME);
+        UserStoreManager userStoreManager = (UserStoreManager) properties.get(IdentityMgtConstants.EventProperty.USER_STORE_MANAGER);
+        UserIdentityDataStore module = (UserIdentityDataStore) properties.get(IdentityMgtConstants.EventProperty.MODULE);
+        IdentityMgtConfig config = (IdentityMgtConfig) properties.get(IdentityMgtConstants.EventProperty.IDENTITY_MGT_CONFIG);
+
+        IdentityUtil.clearIdentityErrorMsg();
+
+        if (!config.isEnableAuthPolicy()) {
+            return true;
+        }
+
+        String domainName = userStoreManager.getRealmConfiguration().getUserStoreProperty(UserCoreConstants.RealmConfig.PROPERTY_DOMAIN_NAME);
+        String usernameWithDomain = UserCoreUtil.addDomainToName(userName, domainName);
+        boolean isUserExistInCurrentDomain = false;
+        try {
+            isUserExistInCurrentDomain = userStoreManager.isExistingUser(usernameWithDomain);
+        } catch (UserStoreException e) {
+            throw new IdentityMgtException("Error in accessing user store");
+        }
+
+        if (!isUserExistInCurrentDomain) {
+
+            IdentityErrorMsgContext customErrorMessageContext = new IdentityErrorMsgContext(UserCoreConstants.ErrorCode.USER_DOES_NOT_EXIST);
+            IdentityUtil.setIdentityErrorMsg(customErrorMessageContext);
+
+            if (log.isDebugEnabled()) {
+                log.debug("Username :" + userName + "does not exists in the system, ErrorMessage :" + UserCoreConstants.ErrorCode.USER_DOES_NOT_EXIST);
+            }
+            if (config.isAuthPolicyAccountExistCheck()) {
+                throw new IdentityMgtException(UserCoreConstants.ErrorCode.USER_DOES_NOT_EXIST);
+            }
+        } else {
+
+            UserIdentityClaim userIdentityDTO = module.load(userName, userStoreManager);
+
+            // if the account is locked, should not be able to log in
+            if (userIdentityDTO != null && userIdentityDTO.isAccountLocked()) {
+
+                // If unlock time is specified then unlock the account.
+                if ((userIdentityDTO.getUnlockTime() != 0) && (System.currentTimeMillis() >= userIdentityDTO.getUnlockTime())) {
+
+                    userIdentityDTO.setAccountLock(false);
+                    userIdentityDTO.setUnlockTime(0);
+
+                    try {
+                        module.store(userIdentityDTO, userStoreManager);
+                    } catch (IdentityException e) {
+                        log.error("Error while saving user : " + userName, e);
+                        throw new IdentityMgtException("Error while saving user : " + userName);
+                    }
+                } else {
+                    IdentityErrorMsgContext customErrorMessageContext = new IdentityErrorMsgContext(UserCoreConstants.ErrorCode.USER_IS_LOCKED,
+                            userIdentityDTO.getFailAttempts(), config.getAuthPolicyMaxLoginAttempts());
+                    IdentityUtil.setIdentityErrorMsg(customErrorMessageContext);
+                    String errorMsg = "User account is locked for user : " + userName
+                            + ". cannot login until the account is unlocked ";
+                    throw new IdentityMgtException(UserCoreConstants.ErrorCode.USER_IS_LOCKED + " "
+                            + errorMsg);
+                }
+            }
+        }
+
+        return true;
+    }
+}
