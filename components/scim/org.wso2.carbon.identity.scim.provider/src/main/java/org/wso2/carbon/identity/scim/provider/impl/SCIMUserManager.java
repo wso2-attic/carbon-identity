d3a397e74ad28ff1cbe1f45579dc0f61625426a8
IndunilR <indunil@wso2.com>
2015-09-28 12:28:00 +0530

    Resolved issues

--- components/scim/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
+++ components/scim/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
@@ -610,7 +610,8 @@ public class SCIMUserManager implements UserManager {
                     SCIMGroupHandler scimGroupHandler = new SCIMGroupHandler(carbonUM.getTenantId());
                     scimGroupHandler.deleteGroupAttributes(group.getDisplayName());
                 } catch (UserStoreException | IdentitySCIMException ex) {
-                    throw new CharonException("Error occurred while doing rollback operation of the SCIM table entry for role: " + group.getDisplayName(), ex);
+                    log.error("Error occurred while doing rollback operation of the SCIM table entry for role: " + group.getDisplayName(), ex);
+                    throw new CharonException("Error occurred while doing rollback operation of the SCIM table entry for role: " + group.getDisplayName(), e);
                 }
                 throw new CharonException("Error occurred while adding role : " + group.getDisplayName(), e);
             } catch (IdentitySCIMException e) {
