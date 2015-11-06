3c8be73a2161859deb8f83a4d8003e82f4acd898
IndunilR <indunil@wso2.com>
2015-11-06 20:31:01 +0530

    Code refactoring

--- components/scim/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
+++ components/scim/org.wso2.carbon.identity.scim.provider/src/main/java/org/wso2/carbon/identity/scim/provider/impl/SCIMUserManager.java
@@ -1072,22 +1072,20 @@ public class SCIMUserManager implements UserManager {
 
                 //SCIM request does not have operation attribute for new members need be added hence parsing null
                 List<String> addRequestedMembers = newGroup.getMembersWithDisplayName(null);
-                //List<String> addRequestMembersUpperCaseDomain = new ArrayList<>();
                 List<String> deleteRequestedMembers =
                         newGroup.getMembersWithDisplayName(SCIMConstants.CommonSchemaConstants.OPERATION_DELETE);
-                //List<String> deleteRequestMembersUpperCaseDomain = new ArrayList<>();
 
                 int noOfAddedMembers = addRequestedMembers.size();
                 int noOfDeletedMembers = deleteRequestedMembers.size();
 
-                for(int i=0; i<noOfAddedMembers; i++) {
+                for(int i=0; i< noOfAddedMembers; i++) {
                     String addRequestMemberUpperCaseDomain = UserCoreUtil
                             .addDomainToName(UserCoreUtil.removeDomainFromName(addRequestedMembers.get(i)),
                                     UserCoreUtil.extractDomainFromName(addRequestedMembers.get(i)));
                     addRequestedMembers.add(addRequestMemberUpperCaseDomain);
                 }
 
-                for(int i=0; i<noOfDeletedMembers; i++) {
+                for(int i=0; i< noOfDeletedMembers; i++) {
                     String deleteRequestMemberUpperCaseDomain = UserCoreUtil
                             .addDomainToName(UserCoreUtil.removeDomainFromName(deleteRequestedMembers.get(i)),
                                     UserCoreUtil.extractDomainFromName(deleteRequestedMembers.get(i)));
@@ -1132,14 +1130,14 @@ public class SCIMUserManager implements UserManager {
                     deletedMembers = Arrays.asList(users);
                 }
 
-                for (int i=noOfAddedMembers; i<addRequestedMembers.size(); i++) {
+                for (int i=noOfAddedMembers; i< addRequestedMembers.size(); i++) {
                     if ((!oldMembers.isEmpty()) && oldMembers.contains(addRequestedMembers.get(i))) {
                         continue;
                     }
                     addedMembers.add(addRequestedMembers.get(i));
                 }
 
-                for (int i = noOfDeletedMembers; i<deleteRequestedMembers.size(); i++) {
+                for (int i = noOfDeletedMembers; i< deleteRequestedMembers.size(); i++) {
                     if ((!oldMembers.isEmpty()) && oldMembers.contains(deleteRequestedMembers.get(i))) {
                         deletedMembers.add(deleteRequestedMembers.get(i));
                     } else {
