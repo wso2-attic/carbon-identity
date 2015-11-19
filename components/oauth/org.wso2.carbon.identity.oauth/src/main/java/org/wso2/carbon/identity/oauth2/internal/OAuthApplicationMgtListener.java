8f73430e3ef8ea5a81ce0d863b6a1a30909de76d
lanka@wso2.com <Russian5Kusum#>
2015-11-19 11:47:54 +0530

    populate and remove client secret at listner

--- components/oauth/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/internal/OAuthApplicationMgtListener.java
+++ components/oauth/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/internal/OAuthApplicationMgtListener.java
@@ -5,11 +5,14 @@ import org.wso2.carbon.identity.application.common.model.InboundAuthenticationRe
 import org.wso2.carbon.identity.application.common.model.Property;
 import org.wso2.carbon.identity.application.common.model.ServiceProvider;
 import org.wso2.carbon.identity.application.mgt.listener.AbstractApplicationMgtListener;
+import org.wso2.carbon.identity.oauth.IdentityOAuthAdminException;
+import org.wso2.carbon.identity.oauth.dao.OAuthConsumerDAO;
 
 public class OAuthApplicationMgtListener extends AbstractApplicationMgtListener {
 
     public static final String OAUTH2 = "oauth2";
     public static final String OAUTH2_CONSUMER_SECRET = "oauthConsumerSecret";
+    private String clientSecret;
 
     @Override
     public int getDefaultOrderId() {
@@ -30,14 +33,22 @@ public class OAuthApplicationMgtListener extends AbstractApplicationMgtListener 
 
     public boolean doPostGetApplication(ServiceProvider serviceProvider, String applicationName, String tenantDomain)
             throws IdentityApplicationManagementException {
-        addClientSecret(serviceProvider, applicationName, tenantDomain);
+        try {
+            addClientSecret(serviceProvider, applicationName, tenantDomain);
+        } catch (IdentityOAuthAdminException e) {
+            throw new IdentityApplicationManagementException("Injecting client secret failed.", e);
+        }
         return true;
     }
 
     public boolean doPostGetApplicationByClientId(ServiceProvider serviceProvider, String clientId, String clientType,
                                                   String tenantDomain) throws IdentityApplicationManagementException {
 
-        addClientSecret(serviceProvider, clientType, tenantDomain);
+        try {
+            addClientSecret(serviceProvider, clientType, tenantDomain);
+        } catch (IdentityOAuthAdminException e) {
+            throw new IdentityApplicationManagementException("Injecting client secret failed.", e);
+        }
         return true;
     }
 
@@ -45,45 +56,49 @@ public class OAuthApplicationMgtListener extends AbstractApplicationMgtListener 
         InboundAuthenticationRequestConfig[] inboundRequestConfigs = serviceProvider.getInboundAuthenticationConfig().
                 getInboundAuthenticationRequestConfigs();
         for (InboundAuthenticationRequestConfig inboundRequestConfig : inboundRequestConfigs) {
-            if(inboundRequestConfig.getInboundAuthType().equals(OAUTH2)){
+            if (inboundRequestConfig.getInboundAuthType().equals(OAUTH2)) {
                 Property[] props = inboundRequestConfig.getProperties();
                 for (Property prop : props) {
-                    if(prop.getName().equalsIgnoreCase(OAUTH2_CONSUMER_SECRET)){
+                    if (prop.getName().equalsIgnoreCase(OAUTH2_CONSUMER_SECRET)) {
                         prop.setValue("");
                         continue;   //we are interested only on this property
-                    } else{
+                    } else {
                         //ignore
                     }
                 }
                 continue;// we are interested only on oauth2 config. Only one will be present.
-            }else{
+            } else {
                 //ignore
             }
         }
     }
 
     private ServiceProvider addClientSecret(ServiceProvider serviceProvider, String clientType,
-                                            String tenantDomain) {
+                                            String tenantDomain) throws IdentityOAuthAdminException {
 
         InboundAuthenticationRequestConfig[] inboundRequestConfigs = serviceProvider.getInboundAuthenticationConfig().
                 getInboundAuthenticationRequestConfigs();
         for (InboundAuthenticationRequestConfig inboundRequestConfig : inboundRequestConfigs) {
-            if(inboundRequestConfig.getInboundAuthType().equals(OAUTH2)){
+            if (inboundRequestConfig.getInboundAuthType().equals(OAUTH2)) {
                 Property[] props = inboundRequestConfig.getProperties();
                 for (Property prop : props) {
-                    if(prop.getName().equalsIgnoreCase(OAUTH2_CONSUMER_SECRET)){
-//                        prop.setValue(getClientSecret());
+                    if (prop.getName().equalsIgnoreCase(OAUTH2_CONSUMER_SECRET)) {
+                        prop.setValue(getClientSecret(inboundRequestConfig.getInboundAuthKey()));
                         continue;   //we are interested only on this property
-                    } else{
+                    } else {
                         //ignore
                     }
                 }
                 continue;// we are interested only on oauth2 config. Only one will be present.
-            }else{
+            } else {
                 //ignore
             }
         }
         return serviceProvider;
     }
 
+    private String getClientSecret(String inboundAuthKey) throws IdentityOAuthAdminException {
+        OAuthConsumerDAO dao = new OAuthConsumerDAO();
+        return dao.getOAuthConsumerSecret(inboundAuthKey);
+    }
 }
