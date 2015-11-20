5daaf01436e7535786eeef1c7e3a33b409234bf1
Damith Senanayake <dracusds123@gmail.com>
2015-11-20 08:33:16 +0530

    adding license header

--- components/user-mgt/org.wso2.carbon.user.mgt/src/main/java/org/wso2/carbon/user/mgt/listeners/UserMgtAuditLogger.java
+++ components/user-mgt/org.wso2.carbon.user.mgt/src/main/java/org/wso2/carbon/user/mgt/listeners/UserMgtAuditLogger.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright (c) 2007 WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+
 package org.wso2.carbon.user.mgt.listeners;
 
 import org.apache.commons.logging.Log;
@@ -77,7 +95,7 @@ public class UserMgtAuditLogger extends AbstractIdentityUserOperationEventListen
     }
 
     public boolean doPostUpdateUserListOfRole(String roleName, String[] deletedUsers, String[] newUsers,
-                                            UserStoreManager userStoreManager) throws UserStoreException {
+                                              UserStoreManager userStoreManager) throws UserStoreException {
         audit.info(String.format(AUDIT_MESSAGE, getUser(), "Update Users of Role", roleName,
                 "Users : " + newUsers.toString(), SUCCESS));
         return true;
@@ -92,7 +110,7 @@ public class UserMgtAuditLogger extends AbstractIdentityUserOperationEventListen
     private String getUser() {
         String user = CarbonContext.getThreadLocalCarbonContext().getUsername() + "@" +
                 CarbonContext.getThreadLocalCarbonContext().getTenantDomain();
-        if (user == null){
+        if (user == null) {
             user = "wso2.system";
         }
         return user;
