a0056c81fffc375604346a405c39a5e6b64ba2da
Gayanm <gmgunawardana@gmail.com>
2015-12-01 12:56:19 +0530

    adding correct Copyright year

--- components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/util/IdentityApplicationManagementUtil.java
+++ components/application-mgt/org.wso2.carbon.identity.application.common/src/main/java/org/wso2/carbon/identity/application/common/util/IdentityApplicationManagementUtil.java
@@ -56,7 +56,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.text.Format;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 
 public class IdentityApplicationManagementUtil {
 
