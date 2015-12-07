ff9b4e3c69c266b08ef3a62ca7eb83e20dd181e9
hasanthiP <hasanthidisanayakka@yahoo.com>
2015-12-07 16:10:20 +0530

    Fixing Identity 4201

--- components/oauth/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/oauth/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -247,8 +247,9 @@ public class OAuth2AuthzEndpoint {
                 OAuth2Parameters oauth2Params = sessionDataCacheEntry.getoAuth2Parameters();
                 String consent = request.getParameter("consent");
                 if (consent != null) {
-
                     if (OAuthConstants.Consent.DENY.equals(consent)) {
+                        OpenIDConnectUserRPStore.getInstance().putUserRPToStore(resultFromConsent.getLoggedInUser().toString(),
+                                resultFromConsent.getoAuth2Parameters().getApplicationName(), false);
                         // return an error if user denied
                         String denyResponse = OAuthASResponse.errorResponse(HttpServletResponse.SC_FOUND)
                                 .setError(OAuth2ErrorCodes.ACCESS_DENIED)
