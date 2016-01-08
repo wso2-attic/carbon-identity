1e4f03ea3469208794a83c64a8dd729d3b1ff097
cdwijayarathna <cdwijayarathna@gmail.com>
2016-01-08 10:34:25 +0530

    Code quality improvements for pull/1110

--- components/user-mgt/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/userstore/DeleteClaimWFRequestHandler.java
+++ components/user-mgt/org.wso2.carbon.user.mgt.workflow/src/main/java/org/wso2/carbon/user/mgt/workflow/userstore/DeleteClaimWFRequestHandler.java
@@ -197,21 +197,16 @@ public class DeleteClaimWFRequestHandler extends AbstractWorkflowRequestHandler 
             try {
                 if (entities[i].getEntityType() == UserStoreWFConstants.ENTITY_TYPE_USER && workflowService
                         .entityHasPendingWorkflowsOfType(entities[i], UserStoreWFConstants.DELETE_USER_EVENT)) {
-
                     throw new WorkflowException("User has a delete operation pending.");
-
                 } else if (entities[i].getEntityType() == UserStoreWFConstants.ENTITY_TYPE_USER && !userStoreManager
                         .isExistingUser(entities[i].getEntityId())) {
-
                     throw new WorkflowException("User " + entities[i].getEntityId() + " does not exist.");
                 }
 
                 if (entities[i].getEntityType() == UserStoreWFConstants.ENTITY_TYPE_USER) {
                     for (int j = 0; j < entities.length; j++) {
-
                         if (entities[j].getEntityType() == UserStoreWFConstants.ENTITY_TYPE_CLAIM && workflowService
                                 .areTwoEntitiesRelated(entities[i], entities[j])) {
-
                             throw new WorkflowException(entities[j].getEntityId() + " of user is already in a " +
                                     "workflow to delete or update.");
                         }
