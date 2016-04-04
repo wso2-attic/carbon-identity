56c20f10e188442a7f8f0ebbcf93712d0f52cc39
Abimaran Kugathasan <abimaran@wso2.com>
2016-04-04 10:31:52 +0530

    Source code formatted

--- components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/model/CookieBuilder.java
+++ components/identity-core/org.wso2.carbon.identity.core/src/main/java/org/wso2/carbon/identity/core/model/CookieBuilder.java
@@ -23,59 +23,59 @@ import javax.servlet.http.Cookie;
 
 public class CookieBuilder {
 
-        String name;
-        String value;
-        String comment;
-        String domain;
-        int maxAge = -1;
-        String path = "/";
-        boolean secure = true;
-        int version = 0;
-        boolean isHttpOnly = true;
+    private String name;
+    private String value;
+    private String comment;
+    private String domain;
+    private int maxAge = -1;
+    private String path = "/";
+    private boolean secure = true;
+    private int version = 0;
+    private boolean isHttpOnly = true;
 
-        public CookieBuilder(String name, String value) {
-            this.name = name;
-            this.value = value;
-        }
+    public CookieBuilder(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
 
-        public CookieBuilder setComment(String comment) {
-            this.comment = comment;
-            return this;
-        }
+    public CookieBuilder setComment(String comment) {
+        this.comment = comment;
+        return this;
+    }
 
-        public CookieBuilder setDomain(String domain) {
-            this.domain = domain;
-            return this;
-        }
+    public CookieBuilder setDomain(String domain) {
+        this.domain = domain;
+        return this;
+    }
 
-        public CookieBuilder setMaxAge(int maxAge) {
-            this.maxAge = maxAge;
-            return this;
-        }
+    public CookieBuilder setMaxAge(int maxAge) {
+        this.maxAge = maxAge;
+        return this;
+    }
 
-        public CookieBuilder setPath(String path) {
-            this.path = path;
-            return this;
-        }
+    public CookieBuilder setPath(String path) {
+        this.path = path;
+        return this;
+    }
 
-        public CookieBuilder setSecure(boolean secure) {
-            this.secure = secure;
-            return this;
-        }
+    public CookieBuilder setSecure(boolean secure) {
+        this.secure = secure;
+        return this;
+    }
 
-        public CookieBuilder setVersion(int version) {
-            this.version = version;
-            return this;
-        }
+    public CookieBuilder setVersion(int version) {
+        this.version = version;
+        return this;
+    }
 
-        public CookieBuilder setHttpOnly(boolean isHttpOnly) {
-            this.isHttpOnly = isHttpOnly;
-            return this;
-        }
+    public CookieBuilder setHttpOnly(boolean isHttpOnly) {
+        this.isHttpOnly = isHttpOnly;
+        return this;
+    }
 
-        public Cookie build() {
-            return new IdentityCookie(this);
-        }
+    public Cookie build() {
+        return new IdentityCookie(this);
+    }
 
     private static class IdentityCookie extends Cookie  {
         private IdentityCookie(CookieBuilder builder)   {
